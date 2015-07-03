febaf262166a1e978c312637e231c29351c9fe3b
Neo Choi <neo.choi@inchcape.com.hk>
2015-07-03 13:50:53 +0800

    let the port number be configable

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // app.js
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 function sendkey(code, callback) {
  /*
@@ -28,7 +29,7 @@ app.get('/left', function (req, res) {
   });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
 
   var host = server.address().address;
   var port = server.address().port;
