5c59bba3290c3895f3f372460bb9f407de1788a9
Neo Choi <neo.choi@inchcape.com.hk>
2015-07-03 13:39:20 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// app.js
+var express = require('express');
+var app = express();
+
+function sendkey(code, callback) {
+ /*
+ var exec = require('child_process').exec;
+ exec("osascript -e 'tell application \"keynote\" to activate\'", function(err, stdout, stderr) {
+ exec("osascript -e 'tell application \"system events\" to key code " + code + "'", function(err, stdout, stderr) {
+ callback();
+ });
+ });
+*/
+  callback();
+}
+
+app.get('/right', function (req, res) {
+  console.log("right");
+  sendkey(124, function() {
+    res.sendStatus(200);
+  });
+});
+
+app.get('/left', function (req, res) {
+  console.log("left");
+  sendkey(123, function() {
+    res.sendStatus(200);
+  });
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
